9a50e6f93e315ef70ac97300db033a3b2ba490ef
Gokul1909 <gokul@terzocloud.com>
2023-06-16 12:13:47 +0530

    Variable Capture

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
 public class Main {
     public static void main(String args[]){
         show();
-        show1();
     }
+    static int number=0;
     public static void show(){
-        greet((message,num)-> System.out.println(message+num)); //Lambda expression instead of anonymous inner class
-    }
-
-    public static void show1(){   //Anonymous inner class to avoid use of separate class
-        Printer print=(message,num) -> System.out.println(message+num);  //Lambda expressions are objects
-        print.consolePrinter("Hi",12);
+        greet((message,num)-> System.out.println(message+num+number)); //static variables can be accessed in lambda
         greet(new Printer() {
             @Override
-            public void consolePrinter(String message,int num) {
-                System.out.println(message+num);
+            public void consolePrinter(String message, int num) {
+                System.out.println(message+number);  //In anonymous inner class also it can be accessed
             }
         });
     }
+
     public static void greet(Printer print){
         print.consolePrinter("Hello World",12);
     }
 }
+
+//In lambda, this keyword refers to the object of the class Main here.
+//In anonymous inner function, it refers to the anonymous class
